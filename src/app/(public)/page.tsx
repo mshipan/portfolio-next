8693666da12ff997a13061a1ec5ff24bebfaec1b
mshipan <mshipan657@gmail.com>
2025-10-17 07:57:20 +0600

    education section created

--- src/app/(public)/page.tsx
+++ src/app/(public)/page.tsx
@@ -2,6 +2,7 @@ import AboutMe from "@/components/modules/homepage/AboutMe";
 import Banner from "@/components/modules/homepage/Banner";
 import Skills from "@/components/modules/homepage/Skills";
 import Experience from "@/components/modules/homepage/Experience";
+import Education from "@/components/modules/homepage/Education";
 
 const HomePage = () => {
   return (
@@ -11,6 +12,7 @@ const HomePage = () => {
       <Skills />
       {/* <AboutMe /> */}
       <Experience />
+      <Education />
     </div>
   );
 };
---
+++ src/components/modules/homepage/Education.tsx
@@ -0,0 +1,15 @@
+import HeaderSection from "@/components/shared/HeaderSection";
+
+const Education = () => {
+  return (
+    <div>
+      <HeaderSection
+        titleSecondPart="education"
+        subTitle="Academic background and achievements"
+      />
+      <div></div>
+    </div>
+  );
+};
+
+export default Education;
