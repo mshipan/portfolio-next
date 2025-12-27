6e0ffbe4f8acd35a234e4878bcbc8782685968f0
mshipan <mshipan657@gmail.com>
2025-12-27 09:04:46 +0600

    make comment on middleware for future use

--- src/middleware.ts
+++ src/middleware.ts
@@ -13,3 +13,35 @@ export function middleware(req: NextRequest) {
 export const config = {
   matcher: ["/dashboard/:path*", "/auth/:path*"],
 };
+
+// the below code, this causing conflicts if my front end and backend both links are different.
+// it will work when the links will like -
+
+// frontend: www.shipan-mallik.com
+// and
+// backend: api.shipan-mallik.com
+
+// -------------------------------------------------
+// JUST SAVE THIS CODE AND COMMENT IT FOR FUTURE USE
+// -------------------------------------------------
+
+// export function middleware(req: NextRequest) {
+//   const accessToken = req.cookies.get("accessToken")?.value;
+//   const pathname = req.nextUrl.pathname;
+
+//   if (pathname.startsWith("/dashboard")) {
+//     if (!accessToken) {
+//       return NextResponse.redirect(new URL("/unauthorized", req.url));
+//     }
+//   }
+
+//   if (pathname.startsWith("/auth") && accessToken) {
+//     return NextResponse.redirect(new URL("/dashboard", req.url));
+//   }
+
+//   return NextResponse.next();
+// }
+
+// export const config = {
+//   matcher: ["/dashboard/:path*", "/auth"],
+// };
